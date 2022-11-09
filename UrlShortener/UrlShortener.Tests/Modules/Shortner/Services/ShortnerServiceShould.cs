5e392aa465c531a1a62c9f8b125d8dd6626e6159
Mike van Oorschot <michael.oorschot@codurance.com>
2022-11-09 16:52:28 +0000

    Corrected ShortnerService tests so they better represent the public interface of the service.

--- UrlShortener/UrlShortener.Tests/Modules/Shortner/Services/ShortnerServiceShould.cs
+++ UrlShortener/UrlShortener.Tests/Modules/Shortner/Services/ShortnerServiceShould.cs
@@ -11,31 +11,14 @@ public class ShortnerServiceShould
 {
     [Theory]
     [InlineAutoMoqData("https://www.google.co.uk/")]
-    public async Task Return_A_Key_Of_The_Configured_Length(
-        string url,
-        [Frozen] ShortnerSettings settings,
-        [Frozen] Mock<IKeyGenerator> keyGenerator,
-        ShortnerService sut)
-    {
-        var expectedKey = new string('X', settings.KeyLength);
-        keyGenerator.Setup(x => x.CreateKey(It.IsAny<int>()))
-            .Returns(expectedKey);
-
-        var actualKey = await sut.CreateShortenedUrl(url);
-        
-        actualKey.Should().Be(expectedKey);
-        keyGenerator.Verify(x => x.CreateKey(settings.KeyLength), Times.Once);
-    }
-    
-    [Theory]
-    [InlineAutoMoqData("https://www.google.co.uk/")]
     public async Task Create_A_Shortened_Url_Entry(
         string url,
-        string expectedId,
+        [Frozen] ShortnerSettings settings,
         [Frozen] Mock<IKeyGenerator> keyGenerator,
         [Frozen] Mock<IShortnerRepository> repository,
         ShortnerService sut)
     {
+        var expectedId = new string('X', settings.KeyLength);
         keyGenerator.Setup(x => x.CreateKey(It.IsAny<int>()))
             .Returns(expectedId);
         
@@ -45,6 +28,7 @@ public class ShortnerServiceShould
         var actualKey = await sut.CreateShortenedUrl(url);
         
         actualKey.Should().Be(expectedId);
+        keyGenerator.Verify(x => x.CreateKey(settings.KeyLength), Times.Once);
         repository.Verify(x => x.CreateEntry(
             It.Is<ShortnerEntry>(entry => entry.Id == expectedId && entry.Url == url)),
             Times.Once);
@@ -53,7 +37,7 @@ public class ShortnerServiceShould
     
     [Theory]
     [InlineAutoMoqData]
-    public async Task Return_The_Correct_Url_For_A_Previously_Created_Key(
+    public async Task Return_The_Correct_Url_For_A_Previously_Created_Entry(
         ShortnerEntry entry,
         [Frozen] Mock<IShortnerRepository> repository,
         ShortnerService sut)
@@ -64,6 +48,22 @@ public class ShortnerServiceShould
         var actualUrl = await sut.GetOriginalUrl(entry.Id);
         
         actualUrl.Should().Be(entry.Url);
+        repository.Verify(x => x.GetEntry(entry.Id), Times.Once);
+    }
+    
+    [Theory]
+    [InlineAutoMoqData]
+    public async Task Return_Null_For_An_Unknown_Entry(
+        ShortnerEntry entry,
+        [Frozen] Mock<IShortnerRepository> repository,
+        ShortnerService sut)
+    {
+        repository.Setup(x => x.GetEntry(It.IsAny<string>()))
+            .ReturnsAsync(null as ShortnerEntry);
+
+        var actualUrl = await sut.GetOriginalUrl(entry.Id);
+        
+        actualUrl.Should().BeNull();
         repository.Verify(x => x.GetEntry(entry.Id), Times.Once);
     }
 }
