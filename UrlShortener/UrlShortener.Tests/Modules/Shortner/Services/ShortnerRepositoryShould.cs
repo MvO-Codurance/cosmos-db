4258581a7c5134b940a30da2d7febff5c8a5573d
Mike van Oorschot <michael.oorschot@codurance.com>
2022-11-09 10:09:52 +0000

    Tidied up some shortner repository tests.

--- UrlShortener/UrlShortener.Tests/Modules/Shortner/Services/ShortnerRepositoryShould.cs
+++ UrlShortener/UrlShortener.Tests/Modules/Shortner/Services/ShortnerRepositoryShould.cs
@@ -16,24 +16,20 @@ public class ShortnerRepositoryShould
     public async Task Create_A_New_Shortner_Entry(
         ShortnerEntry entry)
     {
-        var settings = GetSettingsForEmulator();
-        var cosmosClient = CreateCosmosClient(settings);
-        var sut = new ShortnerRepository(settings, cosmosClient);
+        var sut = GetShortnerRepository();
         
-        var actual = await sut.CreateEntry(entry);
+        var actualId = await sut.CreateEntry(entry);
 
-        actual.Should().NotBeNull();
-        actual.Should().Be(entry.Id);
+        actualId.Should().NotBeNull();
+        actualId.Should().Be(entry.Id);
     }
     
     [Theory]
     [InlineAutoNSubstituteData]
-    public async Task Retrieve_A_Previously_Created_Entry_Using_The_Key(
+    public async Task Retrieve_A_Previously_Created_Entry_Using_The_Id(
         ShortnerEntry entry)
     {
-        var settings = GetSettingsForEmulator();
-        var cosmosClient = CreateCosmosClient(settings);
-        var sut = new ShortnerRepository(settings, cosmosClient);
+        var sut = GetShortnerRepository();
         
         var storedEntryId = await sut.CreateEntry(entry);
         storedEntryId.Should().NotBeNull();
@@ -44,15 +40,20 @@ public class ShortnerRepositoryShould
     
     [Theory]
     [InlineAutoNSubstituteData]
-    public async Task Retrieve_Null_For_An_Unknown_Key(
+    public async Task Retrieve_Null_For_An_Unknown_Id(
         ShortnerEntry entry)
+    {
+        var sut = GetShortnerRepository();
+
+        var actualEntry = await sut.GetEntry(entry.Id);
+        actualEntry.Should().BeNull();
+    }
+
+    private static ShortnerRepository GetShortnerRepository()
     {
         var settings = GetSettingsForEmulator();
         var cosmosClient = CreateCosmosClient(settings);
-        var sut = new ShortnerRepository(settings, cosmosClient);
-        
-        var actualEntry = await sut.GetEntry(entry.Id);
-        actualEntry.Should().BeNull();
+        return new ShortnerRepository(settings, cosmosClient);
     }
 
     private static ShortnerSettings GetSettingsForEmulator()
